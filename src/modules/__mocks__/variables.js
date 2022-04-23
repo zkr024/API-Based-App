5625242964647a3c65a561390003b476fa272a41
Oscar Bermudez <zkr.bm.02403@gmail.com>
2022-04-22 20:00:58 -0500

    Delete src directory

--- src/modules/__mocks__/variables.js
+++
@@ -1,9 +0,0 @@
-document.body.innerHTML = '<p id="count"></p>';
-
-class Variables {
-  count = document.getElementById('count');
-}
-
-const variables = new Variables();
-
-export default variables;
